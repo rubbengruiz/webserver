862211f0adc367f7f352cd9f607d9c5e08284ed8
Ruben <rubben.gruiz@gmail.com>
2019-04-07 12:30:38 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers.js');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res) {
+    res.render('home', {
+        nombre: 'ruben rRuiz'
+    });
+});
+
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando el puerto ${port}`);
+})
